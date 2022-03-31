9bbc8443bbd91b3b27a4b765be7137cf791cf9bc
Paulo Salvatore <salvatoregames@gmail.com>
2022-03-31 10:07:55 -0300

    Funcionamento do `EditText`

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_29_03_2022/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_29_03_2022/MainActivity.kt
@@ -3,6 +3,7 @@ package com.oceanbrasil.ocean_android_introducao_29_03_2022
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +14,16 @@ class MainActivity : AppCompatActivity() {
         // Encontra o elemento btEnviar e armazena na variável btEnviar
         val btEnviar = findViewById<Button>(R.id.btEnviar)
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val etNome = findViewById<EditText>(R.id.etNome)
 
         // Cria um listener para o botão 'Enviar'
         btEnviar.setOnClickListener {
-            // Esse código será executado quando acontecer um clique no botão
-            tvResultado.text = "Paulo Salvatore"
+            if (etNome.text.isNotBlank()) {
+                // Esse código será executado quando acontecer um clique no botão
+                tvResultado.text = etNome.text
+            } else {
+                etNome.error = "Digite um nome válido"
+            }
         }
     }
 }
